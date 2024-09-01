3f04b370ba00e09a3a74cf6e7f3ad54df5b8e891
Naresh <99949786+NareshGarfield@users.noreply.github.com>
2024-09-01 15:33:48 +0530

    Update page.js

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -25,7 +25,7 @@ export default function Home() {
            <div className="relative w-full h-screen flex flex-col items-center justify-center">
            <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2
             -translate-x-1/2 -translate-y-1/2">
-                <h1 className="font-bold text-6xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent">Nareshkumar</h1>
+                <h1 className="font-bold text-5xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent">Nareshkumar</h1>
                   <p className="font-bold text-foreground text-lg">Welcome to my Grand Line!</p>
             </div>
            </div>
